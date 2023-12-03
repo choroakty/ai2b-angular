3d90edd72db5c2315dc2150597aa7e7771cd4aed
choroakty <czechowicz.kacper@o2.pl>
2023-12-03 13:46:37 +0100

    lab-c done

---
+++ lab-c/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ListComponent } from './list/list.component';
+import { DetailsComponent } from './details/details.component';
+import { AddPersonComponent } from './add-person/add-person.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { PersonLsService } from './person-ls.service';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListComponent,
+    DetailsComponent,
+    AddPersonComponent,
+    NotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [
+    PersonLsService,
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
